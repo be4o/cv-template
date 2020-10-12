f7f38d009e39626c347111d5b6e6dd7a34a26596
Beshoy Nabeih <beshoybosha231@gmail.com>
2020-10-12 23:57:21 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+    $("#s-html").animate({
+        width:'90%'
+    }, 500)
+    $("#s-css").animate({
+        width:'70%'
+    }, 500)
+    $("#s-sadmin").animate({
+        width:'60%'
+    }, 500)
+    $("#s-php").animate({
+        width:'95%'
+    }, 800)
+})
